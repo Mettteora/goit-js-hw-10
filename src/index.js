d04fc59c3a48088a8d40d2bcaf04963910c3fbbf
Mettteora <mrhens2013@gmail.com>
2023-04-12 18:06:21 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const promise = new Promise((resolve, reject) => {
+  const canFull = Math.random() > 1;
+
+  setTimeout(() => {
+    if (canFull) {
+      resolve(`Сработал пропис`);
+    }
+    reject(`промис реджектнулся ошибка ебаный рот`);
+  }, 3000);
+});
+
+function ready(resolve) {
+  console.log(`здарвоа это ${resolve}`);
+}
+
+function fail(reject) {
+  console.log(`ошибка бля ${reject}`);
+}
