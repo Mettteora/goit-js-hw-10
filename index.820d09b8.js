65ec7f89545f97677cdafce7a6c38963a44f74ae
Mettteora <95878092+Mettteora@users.noreply.github.com>
2023-04-12 16:06:45 +0000

    Deploying to gh-pages from @ Mettteora/goit-js-hw-10@d04fc59c3a48088a8d40d2bcaf04963910c3fbbf 🚀

---
+++ index.820d09b8.js
@@ -0,0 +1,2 @@
+new Promise((function(n,e){var o=Math.random()>1;setTimeout((function(){o&&n("Сработал пропис"),e("промис реджектнулся ошибка ебаный рот")}),3e3)}));
+//# sourceMappingURL=index.820d09b8.js.map
